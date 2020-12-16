c36db495c4037dd134c331c6f256f7a95b8990cf
mike <jmj00@ucsb.edu>
2020-12-16 14:55:30 -0800

    first commit

---
+++ R/subsetter.R
@@ -0,0 +1,265 @@
+#' @title WRF-Hydro Subsetter
+#' @description Create a cutout of the NWM domain files
+#' @param AOI sf POLYGON. The area to cut out
+#' @param domain_files character. A path to the local domain files
+#' @param outDir Where to put the cutout data should be a named directory!
+#' @return files on disk
+#' @export
+#' @importFrom sf st_bbox st_transform st_as_sf as_Spatial
+#' @importFrom raster rowColFromCell cellFromXY cellFromRowCol xyFromCell
+#' @importFrom dplyr filter mutate left_join group_by summarize
+
+subset_wrf_hydro_domain = function(AOI, domain_files, outDir){
+
+  # create the direcotry if does not exist.
+  myPath = paste0(outDir, "/")
+  dir.create(myPath, showWarnings = FALSE, recursive = TRUE)
+
+#******  Specify the path to the ORIGINAL (full extent) domain files:
+  fullHydFile      <- paste0(domain_files, "/Fulldom_hires_netcdf_1km.nc")
+  fullGeoFile      <- paste0(domain_files, "/geo_em.d01_1km.nc")
+  fullWrfFile      <- paste0(domain_files, "/wrfinput_d01_1km.nc")
+  fullRtlinkFile   <- paste0(domain_files, "/RouteLink_NHDPLUS.nc")
+  fullSpwtFile     <- paste0(domain_files, "/spatialweights_1km_all_basins.nc")
+  fullGwbuckFile   <- paste0(domain_files, "/GWBUCKPARM_CONUS.nc")
+  fullSoilparmFile <- paste0(domain_files, "/soil_veg_properties_LongRange.nc")
+  fullHydro2dFile  <- paste0(domain_files, "/HYDRO_TBL_2D.nc")
+  geoSpatialFile   <- paste0(domain_files, "/WRF_Hydro_NWM_geospatial_data_template_land_GIS.nc")
+
+  coordProj = geo_grid_proj(fullGeoFile)
+
+  # Multiplier between routing grid and LSM grid
+  # (e.g., 1-km LSM and 250-m routing means a value of 4)
+  dxy <- 1
+
+  # Specify the NEW (subset extent) domain files:
+  # Routing domain file
+  subHydFile        <- paste0(myPath, "/Fulldom_hires.nc")
+  subGeoFile        <- paste0(myPath, "/geo_em.d0x.nc")
+  subWrfFile        <- paste0(myPath, "/wrfinput_d0x.nc")
+  subRtlinkFile     <- paste0(myPath, "/Route_Link.nc")
+  subSpwtFile       <- paste0(myPath, "/spatialweights.nc")
+  subGwbuckFile     <- paste0(myPath, "/GWBUCKPARM.nc")
+  subSoilparmFile   <- paste0(myPath, "/soil_properties.nc")
+  subHydro2dFile    <- paste0(myPath, "/hydro2dtbl.nc")
+  subGeoSpatialFile <- paste0(myPath, "/GEOGRID_LDASOUT_Spatial_Metadata.nc")
+
+
+  if(
+    all(file.exists(c(subHydFile, subGeoFile,
+                  subWrfFile, subRtlinkFile,
+                  subSpwtFile, subGwbuckFile,
+                  subSoilparmFile, subHydro2dFile,
+                  subGeoSpatialFile)))) {
+   return(myPath)
+  }
+
+  # Setup coordinates df
+  bb     <- st_bbox(st_transform(AOI, coordProj))
+  coords <- data.frame(id=c(1,2,3,4),
+                       lat=c(bb$ymin, bb$ymax, bb$ymax, bb$ymin),
+                       lon=c( bb$xmin,  bb$xmin, bb$xmax, bb$xmax))
+
+  # Generate spatial coords
+  bb_pts  = st_as_sf(coords, coords = c("lon", 'lat'), crs = coordProj)
+
+  r = make_empty_geogrid_raster(fullGeoFile)
+
+  geoindex <- as.data.frame(raster::rowColFromCell(r, raster::cellFromXY(r, as_Spatial(bb_pts))))
+
+  geoindex$we <- geoindex$col
+  geoindex$sn <- nrow(r) - geoindex$row + 1
+  geoindex$id <- coords[,"id"]
+
+  # Get subsetting dimensions
+  geo_w <- min(geoindex[,"we"])
+  geo_e <- max(geoindex[,"we"])
+  geo_s <- min(geoindex[,"sn"])
+  geo_n <- max(geoindex[,"sn"])
+
+  hyd_w <- (geo_w-1)*dxy+1
+  hyd_e <- geo_e*dxy
+  hyd_s <- (geo_s-1)*dxy+1
+  hyd_n <- geo_n*dxy
+
+  hyd_min <- (min(geoindex$row)-1)*dxy+1
+  hyd_max <- max(geoindex$row)*dxy
+  geo_min <- min(geoindex$row)
+  geo_max <- max(geoindex$row)
+
+  sp_new_buff_nad83 <- st_as_sf(xyFromCell(r,
+                                           cellFromRowCol(r,
+                                                          c(geo_max, geo_min, geo_min, geo_max),
+                                                          c(geo_w, geo_w, geo_e, geo_e)), spatial=TRUE)) %>%
+    st_transform("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs") %>%
+    st_coordinates()
+
+################# SUBSET DOMAIN Files
+  # ROUTING GRID
+  if (!is.null(fullHydFile)) {
+    if  (!file.exists(fullHydFile)) stop(paste0("The fullHydFile : ", fullHydFile, " does not exits"))
+    system(paste0("ncks -O -d x,", hyd_w-1, ",", hyd_e-1, " -d y,", hyd_min-1, ",", hyd_max-1, " ", fullHydFile, " ", subHydFile))
+  }
+
+  # Geo Spatial File
+  if (!is.null(geoSpatialFile)) {
+    if (!file.exists(geoSpatialFile)) stop(paste0("The geoSpatialFile :", geoSpatialFile, " does not exits"))
+    system(paste0("ncks -O -d x,", geo_w-1, ",",
+                  geo_e-1, " -d y,", geo_s-1, ",",
+                  geo_n-1, " ", geoSpatialFile, " ",
+                  subGeoSpatialFile))
+    }
+
+
+  # GEO GRID
+  if (!is.null(fullGeoFile)) {
+    if (!file.exists(fullGeoFile)) stop(paste0("The fullGeoFile :", fullGeoFile, " does not exits"))
+    system(paste0("ncks -O -d west_east,", geo_w-1, ",", geo_e-1, " -d south_north,", geo_s-1, ",", geo_n-1,
+                  " -d west_east_stag,", geo_w-1, ",", geo_e, " -d south_north_stag,",geo_s-1, ",", geo_n, " ",
+                  fullGeoFile, " ", subGeoFile))
+  }
+
+  corner_lats <- c()
+
+  for (latName in c('XLAT_M', 'XLAT_U', 'XLAT_V', 'XLAT_C')) {
+    if (latName %in% var_names(subGeoFile)) {
+      a = RNetCDF::var.get.nc(RNetCDF::open.nc(subGeoFile), latName)
+      corners = c(a[1,1], a[1, ncol(a)], a[nrow(a), ncol(a)], a[nrow(a), 1])
+    }else{
+      corners = c(0,0,0,0)
+    }
+    corner_lats = c(corner_lats, corners)
+  }
+
+  corner_lons <- c()
+  for (lngName in c('XLONG_M', 'XLONG_U', 'XLONG_V', 'XLONG_C')) {
+    if (lngName %in% var_names(subGeoFile)) {
+      a = RNetCDF::var.get.nc(RNetCDF::open.nc(subGeoFile), lngName)
+      corners = c(a[1,1], a[1, ncol(a)], a[nrow(a), ncol(a)], a[nrow(a), 1])
+    }else{
+      corners = c(0,0,0,0)
+    }
+    corner_lons = c(corner_lons, corners)
+  }
+
+  # Attribute updates
+  system(paste0("ncatted -h -a WEST-EAST_GRID_DIMENSION,global,o,l,", geo_e-geo_w+2, " ", subGeoFile))
+  system(paste0("ncatted -h -a SOUTH-NORTH_GRID_DIMENSION,global,o,l,", geo_n-geo_s+2, " ", subGeoFile))
+  system(paste0("ncatted -h -a WEST-EAST_PATCH_END_UNSTAG,global,o,l,", geo_e-geo_w+1, " ", subGeoFile))
+  system(paste0("ncatted -h -a SOUTH-NORTH_PATCH_END_UNSTAG,global,o,l,", geo_n-geo_s+1, " ", subGeoFile))
+  system(paste0("ncatted -h -a WEST-EAST_PATCH_START_STAG,global,d,,, ", subGeoFile))
+  system(paste0("ncatted -h -a SOUTH-NORTH_PATCH_START_STAG,global,d,,, ", subGeoFile))
+  system(paste0("ncatted -h -a WEST-EAST_PATCH_END_STAG,global,d,,, ", subGeoFile))
+  system(paste0("ncatted -h -a SOUTH-NORTH_PATCH_END_STAG,global,d,,, ", subGeoFile))
+  system(paste0("ncatted -h -a i_parent_end,global,o,l,", geo_e-geo_w+2, " ", subGeoFile))
+  system(paste0("ncatted -h -a j_parent_end,global,o,l,", geo_n-geo_s+2, " ", subGeoFile))
+  system(paste0("ncatted -O -a corner_lons,global,o,f,", paste(corner_lons, collapse  = ","), " ", subGeoFile))
+  system(paste0("ncatted -O -a corner_lats,global,o,f,", paste(corner_lats, collapse  = ","), " ", subGeoFile))
+
+  #HYDRO_TBL_2D GRID
+  if (!is.null(fullHydro2dFile)) {
+    if (!file.exists(fullHydro2dFile)) stop(paste0("The fullHydro2dFile : ", fullHydro2dFile, " does not exits"))
+    system(paste0("ncks -O -d west_east,", geo_w-1, ",", geo_e-1, " -d south_north,", geo_s-1, ",", geo_n-1, " ", fullHydro2dFile, " ", subHydro2dFile))
+  }
+
+  # WRFINPUT GRID
+
+  if (!is.null(fullWrfFile)) {
+    if (!file.exists(fullWrfFile)) stop(paste0("The fullWrfFile : ", fullWrfFile, " does not exits"))
+    system(paste0("ncks -O -d west_east,", geo_w-1, ",", geo_e-1, " -d south_north,", geo_s-1, ",", geo_n-1, " ", fullWrfFile, " ", subWrfFile))
+    system(paste0("ncatted -h -a WEST-EAST_GRID_DIMENSION,global,o,l,", geo_e-geo_w+2, " ", subWrfFile))
+    system(paste0("ncatted -h -a SOUTH-NORTH_GRID_DIMENSION,global,o,l,", geo_n-geo_s+2, " ", subWrfFile))
+  }
+
+  ################# SUBSET PARAMS
+if (!file.exists(fullSpwtFile)) stop(paste0("The fullSpwtFile : ", fullSpwtFile, " does not exits"))
+if (!file.exists(fullRtlinkFile)) stop(paste0("The fullRtlinkFile : ", fullRtlinkFile, " does not exits"))
+
+    SpatWts <- read_wt_file(fullSpwtFile)
+
+    keepIdsPoly <-  dplyr::filter(SpatWts$data,
+                         i_index >= hyd_w &
+                         i_index <= hyd_e &
+                         j_index >= hyd_s &
+                         j_index <= hyd_n) %>%
+      dplyr::group_by(IDmask) %>%
+      dplyr::summarise(sumBas = sum(weight)) %>%
+      dplyr::filter(sumBas > .999) %>%
+      dplyr::pull(IDmask)
+
+    rt_link = nc_to_df(fullRtlinkFile)
+
+    keepIdsLink = rt_link %>%
+      dplyr::filter(
+          lon >= min(sp_new_buff_nad83[,1]) &
+          lon <= max(sp_new_buff_nad83[,1]) &
+          lat >= min(sp_new_buff_nad83[,2]) &
+          lat <= max(sp_new_buff_nad83[,2])) %>%
+      dplyr::distinct(link) %>%
+      dplyr::pull(link)
+
+    keepIds <- unique(c(keepIdsPoly, keepIdsLink))
+
+    # SPATIAL WEIGHT
+    subWts = subset_weights(SpatWts, keepIdsPoly, hyd_w, hyd_e, hyd_s, hyd_n)
+
+    fs::file_copy(fullSpwtFile, subSpwtFile, overwrite = TRUE)
+
+    system(paste0("ncks -O -d polyid,1,", nrow(subWts[[2]]),
+                       " -d data,1,", nrow(subWts[[1]]), " ", subSpwtFile, " ",
+                     subSpwtFile))
+
+    update_nc(subSpwtFile, subWts[[1]])
+    update_nc(subSpwtFile, subWts[[2]])
+
+    # ROUTE LINK
+    subRtlink <- dplyr::filter(rt_link, link %in% keepIds) %>%
+      dplyr::mutate(to = ifelse(to %in% unique(link), to, 0))
+
+    # reorder the ascendingIndex if ascendingIndex exists in the variables
+    if ("ascendingIndex" %in% names(subRtlink)){
+      subRtlink = dplyr::mutate(subRtlink, ascendingIndex = (rank(ascendingIndex) - 1))
+    }
+
+    #rwrfhydro::UpdateLinkFile(subRtlinkFile, subRtlink)
+    fs::file_copy(fullRtlinkFile, subRtlinkFile, overwrite = TRUE)
+
+    system(paste0("ncks -O -d feature_id,1,", nrow(subRtlink), " ",
+                  subRtlinkFile, " ", subRtlinkFile))
+
+    update_nc(path = subRtlinkFile, df = subRtlink)
+
+  # GWBUCK PARAMETER
+
+if (!is.null(fullGwbuckFile)) {
+
+    if (is.null(fullSpwtFile) | is.null(fullRtlinkFile)) {
+      stop("To subset the fullSpwtFile, you need fullSpwtFile and fullRtlinkFile")
+    }
+    if (!file.exists(fullGwbuckFile)) stop(paste0("the fullGwbuckFile : ",
+                                                  fullGwbuckFile, " does not exits"))
+
+   subGwbuck = nc_to_df(fullGwbuckFile) %>%
+      dplyr::filter(ComID %in% keepIdsPoly) %>%
+      dplyr::mutate(Basin = 1:n())
+
+    file.copy(fullGwbuckFile, subGwbuckFile, overwrite = TRUE)
+
+    system(paste0("ncks -O -d BasinDim,1,", nrow(subGwbuck),  " ",
+                  subGwbuckFile, " ", subGwbuckFile))
+
+    update_nc(path = subGwbuckFile, df = subGwbuck)
+
+  }
+
+  # SOIL PARAMETER
+
+  if (!is.null(fullSoilparmFile)) {
+    if (!file.exists(fullSoilparmFile)) stop(paste0("the fullSoilparmFile : ", fullSoilparmFile, " does not exits"))
+    system(paste0("ncks -O -d west_east,", geo_w-1, ",", geo_e-1, " -d south_north,", geo_s-1, ",", geo_n-1, " ", fullSoilparmFile, " ", subSoilparmFile))
+  }
+
+  message("Finshied! All files in: ", myPath)
+
+}
+
---
+++ R/utils.R
@@ -0,0 +1,307 @@
+#' Find Latest Version of NWM on NCEP
+#' @return character
+#' @export
+
+latest_nwm_version = function(){
+  ncep = 'https://www.nco.ncep.noaa.gov/pmb/codes/nwprod/'
+  ver = grep("nwm", readLines(ncep), value = TRUE)
+  gsub('^.*href=\"\\s*|\\s*/.*$', '', ver)
+}
+
+#' @title Download National Water Model Domain Files
+#' @description Download the domain files (CONUS) used in the most
+#' current operational NWM. It will download
+#' ~17GB of data so plan accordingly!
+#' @details This function will download the follwing national (CONUS) domain
+#'  files for the latest version of the National Water Model on NCEP.
+#' \itemize{
+##'  \item{"Fulldom_hires_netcdf_1km.nc"}
+##'  \item{"geo_em.d01_1km.nc"}
+##'  \item{"wrfinput_d01_1km.nc"}
+##'  \item{"RouteLink_NHDPLUS.nc"}
+##'  \item{"spatialweights_1km_all_basins.nc"}
+##'  \item{"GWBUCKPARM_CONUS.nc"}
+##'  \item{"soil_veg_properties_LongRange.nc"}
+##'  \item{"HYDRO_TBL_2D.nc"}
+##'  \item{"WRF_Hydro_NWM_geospatial_data_template_land_GIS.nc"}
+##' }
+##'
+##' This function will NOT download:
+##'\itemize{
+##'  \item{"Fulldom_hires_netcdf_250m.nc"}("~13GB)
+##'  \item{"GWBUCKPARM_CONUS_LongRange.nc"}("92B")
+##'  \item{"HYDRO_TBL_2D_LongRange.nc"}{"338MB"}
+##'  \item{"nudgingParams.nc"}{"2.4MB"}
+##'  \item{"soil_veg_properties_ASM.nc"}{"3GB}
+##'  \item{"soil_veg_properties_LongRange.nc"}{"3GB"}
+##'  \item{"spatialweights_250m_all_basins.nc"}{"4.5GB"}
+##' }
+#' @param outDir where to write the files. Again ~17GB worth!
+#' @importFrom httr GET write_disk progress
+#' @return path to domain file downloads
+#' @export
+
+download_conus_nwm = function(outDir = NULL){
+
+  ver = latest_nwm_version()
+
+  nwmDir = paste0('https://www.nco.ncep.noaa.gov/pmb/codes/nwprod/',
+                  ver,
+                  '/parm/domain')
+
+  needed_files = c("Fulldom_hires_netcdf_1km.nc",
+                   "geo_em.d01_1km.nc",
+                   "wrfinput_d01_1km.nc",
+                   "RouteLink_NHDPLUS.nc",
+                   "spatialweights_1km_all_basins.nc",
+                   "GWBUCKPARM_CONUS.nc",
+                   "soil_veg_properties_LongRange.nc",
+                   "HYDRO_TBL_2D.nc",
+                   "WRF_Hydro_NWM_geospatial_data_template_land_GIS.nc")
+
+  localDir = paste0(outDir, "/nwmCONUS-", gsub("nwm", "", gsub("[.]", "",ver)))
+
+  dir.create(localDir, showWarnings = FALSE, recursive = TRUE)
+
+  urls  = paste(nwmDir, needed_files, sep = "/")
+  local = paste(localDir, needed_files, sep = "/")
+
+  for( i in 1:length(urls)){
+    if(!file.exists(local[i])){
+      message('Downloading: ', basename(local[i]))
+      httr::GET(urls[i], httr::write_disk(local[i]), httr::progress())
+    } else {
+      message(basename(local[i]),   ' already exists')
+    }
+  }
+
+  message("\n\nAll files located: ", localDir)
+  localDir
+}
+
+#' Get GeoGrid Projection
+#' @description From a geogrid file, extract the proj4string.
+#' @details Extracts projection data from global attributes
+#' @param path a path to the local geogrid.nc file
+#' @importFrom RNetCDF open.nc att.get.nc
+#' @return a PROJ4STRING
+#' @export
+
+geo_grid_proj = function(path)
+{
+  suppressWarnings({
+  nc = RNetCDF::open.nc(path)
+  map_proj <- RNetCDF::att.get.nc(nc, "NC_GLOBAL", "MAP_PROJ")
+  cen_lat  <- RNetCDF::att.get.nc(nc, "NC_GLOBAL", "CEN_LAT")
+  cen_lon  <- RNetCDF::att.get.nc(nc, "NC_GLOBAL", "STAND_LON")
+  truelat1 <- RNetCDF::att.get.nc(nc, "NC_GLOBAL", "TRUELAT1")
+  truelat2 <- RNetCDF::att.get.nc(nc, "NC_GLOBAL", "TRUELAT2")
+  if (map_proj == 1) {
+    crs.here <- paste0("+proj=lcc +lat_1=", truelat1,
+                       " +lat_2=", truelat2,
+                       " +lat_0=", cen_lat,
+                       " +lon_0=", cen_lon,
+                       " +x_0=0 +y_0=0 +a=6370000 +b=6370000 +units=m +no_defs")
+  }
+  return(crs.here)
+  })
+}
+
+
+#' Return GeoGrid Raster Structure
+#' @description Given a NetCDF path, a raster object is returned with the
+#' appropriate structure, valeus are NA, unless a 'var' is defined. S
+#' ee var_names for help indetifing availiable varible names
+#' @param path a path to a NetCDF file
+#' @param var an optional variable to extract
+#' @return a rasterLayer
+#' @export
+#' @importFrom RNetCDF open.nc dim.inq.nc att.get.nc var.get.nc
+#' @importFrom sf st_point st_sfc st_transform st_coordinates
+#' @importFrom raster raster values
+
+make_empty_geogrid_raster = function(path, var = NULL)
+{
+  crs.here = geo_grid_proj(path)
+  nc = RNetCDF::open.nc(path)
+  x  = RNetCDF::dim.inq.nc(nc, 'west_east')$length
+  y  = RNetCDF::dim.inq.nc(nc, 'south_north')$length
+  dx = RNetCDF::att.get.nc(nc, "NC_GLOBAL", "DX")
+  dy = RNetCDF::att.get.nc(nc, "NC_GLOBAL", "DY")
+  lat_max = RNetCDF::var.get.nc(nc, "XLAT_M", start = c(1,y,1), count = c(1,1,1))
+  lng_max = RNetCDF::var.get.nc(nc, "XLONG_M", start = c(1,y,1), count = c(1,1,1))
+
+  pts = st_sfc(st_point(c(lng_max,lat_max)), crs = 4326) %>%
+    st_transform(crs.here) %>%
+    st_coordinates()
+
+  xmin = pts[1] - dx/2
+  ymax = pts[2] + dy/2
+  xmax = xmin + x*dx
+  ymin = ymax - y*dy
+
+  suppressWarnings({
+    r = raster(res = c(dx,dy), xmn = xmin, xmx = xmax,
+               ymn = ymin, ymx = ymax,
+               crs = crs.here)
+  })
+
+  if(!is.null(var)){
+    var = RNetCDF::var.get.nc(nc, var)
+    raster::values(r) = apply(t(var),2,rev)
+  }
+
+  r
+}
+
+
+#' @title NetCDF Variable Names from a path
+#' @description Given a path to a NetCDF file, list the variables
+#' @param path a path to a NetCDF file
+#' @return a vectore of variable names
+#' @export
+#' @importFrom RNetCDF open.nc file.inq.nc var.inq.nc
+
+var_names = function(path){
+  nc = RNetCDF::open.nc(path)
+  nvar <- RNetCDF::file.inq.nc(nc)$nvar
+  varnames <- character(nvar)
+  for(i in seq_len(nvar)) {
+    varnames[i] <- RNetCDF::var.inq.nc(nc, i-1)$name
+  }
+  varnames
+}
+
+#' @title NetCDF Dimension Names from a path
+#' @description Given a path to a NetCDF file, list the diminsions
+#' @param path a path to a NetCDF file
+#' @return a vector of dimension names
+#' @export
+#' @importFrom RNetCDF open.nc file.inq.nc var.inq.nc
+
+dim_names = function(path){
+  nc = RNetCDF::open.nc(path)
+  ndim <- RNetCDF::file.inq.nc(nc)$ndim
+  dimnames <- character(ndim)
+  for(i in seq_len(ndim)) {
+    dimnames[i] <- RNetCDF::dim.inq.nc(nc, i-1)$name
+  }
+  dimnames
+}
+
+
+#' @title Re-write of rwfhydro ReadWtFile
+#' @description  reads in a spatial weight file and generates
+#' a list of two dataframes: 1 for the gridded info (data) and 2 for
+#' the poly info (poly)
+#' @param path path to the weight file
+#' @return list of 2 data.frames
+#' @export
+#' @importFrom RNetCDF open.nc file.inq.nc var.inq.nc
+#
+read_wt_file = function (wtFile)
+{
+  nc <- RNetCDF::open.nc(wtFile)
+
+   list(data = data.frame(
+    i_index = RNetCDF::var.get.nc(nc, "i_index"),
+    j_index = RNetCDF::var.get.nc(nc, "j_index"),
+    IDmask  = RNetCDF::var.get.nc(nc, "IDmask"),
+    weight  = RNetCDF::var.get.nc(nc, "weight"),
+    regridweight = RNetCDF::var.get.nc(nc, "regridweight")),
+
+    polys = data.frame(polyid = RNetCDF::var.get.nc(nc, "polyid"),
+                      overlaps = RNetCDF::var.get.nc(nc, "overlaps"))
+  )
+
+}
+
+
+
+#' @title Re-write of rwrfhydro::SubsetWts
+#' @description SubsetWts takes a weight file object (as read in from ReadWtFile)
+#' and subsets the data and poly dataframes based on a user-defined set of link
+#' IDs and user-defined start and end indices. Returns a list of 2 dataframes
+#' for the subsetted data and poly objects.
+#' @param wts Weight file list object
+#' @param rlids Vector of link IDs to keep in the subsetted output
+#' @param istart Start index for the grid in the i (x) direction
+#' @param iend End index for the grid in the i (x) direction
+#' @param jstart Start index for the grid in the j (y) direction
+#' @param jend End index for the grid in the j (y) direction
+#' @return List of 2 dataframes
+#' @export
+
+subset_weights = function (wts, rlids, istart, iend, jstart, jend)
+{
+
+  wtssubrect = dplyr::filter(wts$data,
+                         IDmask %in% rlids) %>%
+    dplyr::mutate(i_index = i_index - (istart - 1),
+           j_index =j_index - (jstart - 1)) %>%
+    dplyr::filter(
+      i_index > 0 &
+      i_index <= (iend - istart + 1) &
+      j_index > 0 &
+      j_index <= (jend - jstart + 1)
+    )
+
+  polywts2 = wtssubrect %>%
+    dplyr::group_by(IDmask) %>%
+    dplyr::summarise(mult = sum(weight))
+
+  wtssubrect = dplyr::left_join(
+    wtssubrect, polywts2, by = "IDmask"
+  ) %>%
+    dplyr::mutate(weight = weight / mult,
+                  mult = NULL)
+
+  polysub = dplyr::filter(wts$polys, polyid %in% rlids)
+  names(polysub) <- c("polyid", "kill")
+
+  tmp = dplyr::count(wtssubrect, IDmask)
+  names(tmp) <- c("polyid", "overlaps")
+
+  polysub <- dplyr::left_join(polysub, tmp, by = "polyid") %>%
+    dplyr::mutate(overlaps = ifelse(is.na(overlaps), 0, overlaps),
+                  kill = NULL)
+
+  return(list(wtssubrect, polysub))
+}
+
+update_nc = function(path, df){
+
+  nc <- RNetCDF::open.nc(path, write = TRUE)
+  for (i in names(df)) {
+    if (i %in% var_names(path)) {
+      if(RNetCDF::var.inq.nc(nc,i)$ndim > 0){
+        print(i)
+        RNetCDF::var.put.nc(nc, var = i, df[[i]])
+      }
+    }
+  }
+  RNetCDF::close.nc(nc)
+
+}
+
+
+
+#' @title Read a NetCDF file into a data.frame
+#' @param path a path to a NetCDF
+#' @return a data.frame
+#' @importFrom dplyr bind_cols
+#' @importFrom RNetCDF open.nc var.get.nc
+#' @export
+
+nc_to_df = function(path){
+  nc = RNetCDF::open.nc(path)
+  vars = var_names(path)
+  out = lapply(1:length(vars), function(i){
+    RNetCDF::var.get.nc(nc, vars[i])
+  })
+
+  out = suppressMessages(dplyr::bind_cols(out))
+  names(out) = vars
+  out
+}
+
